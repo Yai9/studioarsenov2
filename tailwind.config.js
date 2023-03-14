fc85be79aed51ea190ef569ec8b37156e040ae0e
Yai <46352948+Yai9@users.noreply.github.com>
2023-03-14 01:07:16 +0100

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      xs: "400px",
+      nav: "1000px",
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        primary: "#0a0a0a",
+        secondary: "#ffffff",
+        accent: "#000000",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+}
