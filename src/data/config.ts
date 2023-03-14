e601f93a7ca934f0506fa58c8e8a24f2cf2195ac
yai <arsenovlidija02@gmail.com>
2023-03-14 01:18:21 +0100

    Second commit

---
+++ src/data/config.ts
@@ -0,0 +1,53 @@
+export const config = {
+  srcSetSizes: ["256", "512", "768", "1280"],
+  ogImage: "/og-image.png",
+  logo: "/logo.png",
+  siteTitle: "Studio Arsenov",
+  seo: {
+    defaultTitle: "Studio Arsenov",
+    defaultDescription: "Studio Arsenov",
+    contact: {
+      phone: "+381641300913",
+    },
+  },
+  favico: "/favicon.ico",
+  contact: {
+    phone: "+381641300913",
+    email: "studioarsenov@gmail.com",
+    address: `Novosadska 4. 22320 Inđija`,
+  },
+  menu: [
+    {
+      title: "NASLOVNA",
+      url: "/#",
+    },
+    {
+      title: "O NAMA",
+      url: "/#o-nama",
+    },
+    {
+      title: "USLUGE",
+      url: "/#usluge",
+    },
+    {
+      title: "UTISCI",
+      url: "/#utisci",
+    },
+    {
+      title: "KONTAKT",
+      url: "/#kontakt",
+    },
+  ],
+  socials: [
+    {
+      title: "Facebook",
+      url: "https://www.facebook.com/studioarsenov/",
+      icon: "ic:baseline-facebook",
+    },
+    {
+      title: "Instagram",
+      url: "https://www.instagram.com/studio_arsenov/",
+      icon: "mdi:instagram",
+    },
+  ],
+}
